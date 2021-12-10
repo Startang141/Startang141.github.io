48f17576e6a6efab4fd27ed99d9cfa7f2ebfa378
Startang141 <bintangadiyatma141@gmail.com>
2021-12-10 15:24:57 +0700

    first commit

---
+++ insert.php
@@ -0,0 +1,20 @@
+<?php 
+
+include 'config.php';
+
+if (isset($_POST['submit'])) {
+	$email = $_POST['email'];
+	$password = md5($_POST['password']);
+
+	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+	$result = mysqli_query($conn, $sql);
+	if ($result->num_rows > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['username'] = $row['username'];
+		header("Location: tugas.html");
+	} else {
+		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
+	}
+}
+
+?>
